cbc488f0af301d594162d2f236aaa14c3ca0259f
Man Tran <tranduyminhman@gmail.com>
2025-02-17 14:22:32 +0700

    finish menu

--- seashell-react/src/app/components/MenuItem/index.tsx
+++ seashell-react/src/app/components/MenuItem/index.tsx
@@ -1,20 +1,26 @@
 import { Link } from "react-router";
 import "./style.scss";
+import { ReactNode } from "react";
 
 type TMenuItemProps = {
-  id: string;
-  icon: string;
+  icon: ReactNode;
   path: string;
   text: string;
   isActive: boolean;
+  width: number;
 };
 
-function MenuItem({ id, icon, path, text, isActive }: TMenuItemProps) {
+function MenuItem({ icon, path, text, isActive, width }: TMenuItemProps) {
   return (
-    <div className="menu-item">
-      <Link to={path} className={isActive ? "active" : ""}>
-        <img src={icon} alt={id} />
-        <p>{text}</p>
+    <div
+      className={`menu-item ${isActive ? "active" : ""}`}
+      style={{ flexBasis: `${width}px`, height: `${width}px` }}
+    >
+      <Link to={path}>
+        <div className="icon">{icon}</div>
+        <div className="text">
+          <span>{text}</span>
+        </div>
       </Link>
     </div>
   );
