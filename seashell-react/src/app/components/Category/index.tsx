2ef3bbf404de91eabe3baf826ed3ed738d4ae964
Man Tran <tranduyminhman@gmail.com>
2025-02-24 15:40:43 +0700

    update category list

--- seashell-react/src/app/components/Category/index.tsx
+++ seashell-react/src/app/components/Category/index.tsx
@@ -1,6 +1,6 @@
 import { Link, useNavigate } from "react-router";
 import "./style.scss";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { keyboard } from "../../keyboard";
 
 type TCategory = {
@@ -8,11 +8,21 @@ type TCategory = {
   list: any[];
 };
 
+const itemWidth = 400;
+const itemHeight = 163;
+
 function Category({ title, list }: TCategory) {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [currentSlider, setCurrentSlider] = useState(0);
   const [cursor, setCursor] = useState(0);
   const navigate = useNavigate();
+  const trackRef = useRef<HTMLDivElement>(null);
+  const [heightTrack, setHeightTrack] = useState(100);
+  const [translateWidth, setTranslateWidth] = useState(0);
+  const [translateHeight, setTranslateHeight] = useState(0);
+  const [showThumb, setShowThumb] = useState(false);
+  const outerRef = useRef<HTMLDivElement>(null);
+  const innerRef = useRef<HTMLDivElement>(null);
 
   const handleKeyDown = (event: any) => {
     let keycode;
@@ -23,9 +33,21 @@ function Category({ title, list }: TCategory) {
     }
 
     if (keycode === keyboard.RIGHT) {
+      if (translateWidth < itemWidth + 20) {
+        setTranslateWidth(translateWidth + (itemWidth + 20));
+      }
     } else if (keycode === keyboard.LEFT) {
+      if (translateWidth > 0) {
+        setTranslateWidth(translateWidth - (itemWidth + 20));
+      }
     } else if (keycode === keyboard.TOP) {
+      if (translateHeight > 0) {
+        setTranslateHeight(translateHeight - (itemHeight + 20));
+      }
     } else if (keycode === keyboard.BOTTOM) {
+      if (translateHeight < 452 - itemHeight - 20) {
+        setTranslateHeight(translateHeight + (itemHeight + 20));
+      }
     } else if (keycode === keyboard.ENTER) {
     }
   };
@@ -37,15 +59,47 @@ function Category({ title, list }: TCategory) {
     };
   });
 
+  const calculateScroller = () => {
+    if (outerRef.current && innerRef.current) {
+      const { height: outerHeight } = outerRef.current.getBoundingClientRect();
+      const { height: innerHeight } = innerRef.current.getBoundingClientRect();
+      if (outerHeight < innerHeight) {
+        setShowThumb(true);
+        setHeightTrack((outerHeight / innerHeight) * 100);
+      }
+      console.log("outerRef.current", outerRef.current.getBoundingClientRect());
+      console.log("innerRef.current", innerRef.current.getBoundingClientRect());
+    }
+  };
+
+  useEffect(() => {
+    calculateScroller();
+  }, []);
+
   return (
     <div className="category-wrapper">
       <h2>{title}</h2>
-      <div className="category-content">
-        <div className="category-list">
+      <div className="category-content" ref={outerRef}>
+        <div
+          className="category-scroller"
+          style={{ display: showThumb ? "block" : "none" }}
+        >
+          <div style={{ height: `${heightTrack}%` }} />
+        </div>
+        <div
+          className="category-cursor"
+          style={{
+            transform: `translate(${translateWidth}px, ${translateHeight}px)`,
+          }}
+        />
+        <div className="category-list" ref={innerRef}>
           {list.map((item) => {
             return (
               <div className="category-item" key={item.title}>
-                <Link to={item.path}>
+                <Link
+                  to={item.path}
+                  style={{ width: itemWidth, height: itemHeight }}
+                >
                   <img src={item.img} alt="" />
                   <div className="brief">
                     <p className="title">{item.title}</p>
