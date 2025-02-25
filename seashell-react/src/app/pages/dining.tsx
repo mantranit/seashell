87b00e771e05fcc272f04cffebf8fe17f9e99c39
Man Tran <tranduyminhman@gmail.com>
2025-02-25 15:30:10 +0700

    dining

--- seashell-react/src/app/pages/dining.tsx
+++ seashell-react/src/app/pages/dining.tsx
@@ -1,9 +1,90 @@
-import MenuVertical from "../components/MenuVertical";
+import { useState } from "react";
+import Category from "../components/Category";
+import LeftBottomButtons from "../components/LeftBottomButtons";
+import RightBottomButtons from "../components/RightBottomButtons";
+import { t } from "i18next";
+
+import imgBreakfasts from "../../assets/categories/dining/breakfasts.jpg";
+import imgStarters from "../../assets/categories/dining/starters.jpg";
+import imgSalads from "../../assets/categories/dining/salads.jpg";
+import imgSoups from "../../assets/categories/dining/soups.jpg";
+import imgMains from "../../assets/categories/dining/mains.jpg";
+import imgDesserts from "../../assets/categories/dining/desserts.jpg";
+import imgDrinks from "../../assets/categories/dining/drinks.jpg";
+import imgHotDrinks from "../../assets/categories/dining/hot-drinks.jpg";
+import imgAlcohol from "../../assets/categories/dining/alcohol.jpg";
 
 function Dining() {
+  const [list] = useState([
+    {
+      category: [],
+      title: "Breakfasts",
+      subtitle: "07:00-11:00",
+      img: imgBreakfasts,
+      path: "/dining/breakfasts",
+    },
+    {
+      category: [],
+      title: "Starters",
+      subtitle: "07:00-23:00",
+      img: imgStarters,
+      path: "/dining/starters",
+    },
+    {
+      category: [],
+      title: "Salads",
+      subtitle: "07:00-23:00",
+      img: imgSalads,
+      path: "/dining/salads",
+    },
+    {
+      category: [],
+      title: "Soups",
+      subtitle: "10:00-20:00",
+      img: imgSoups,
+      path: "/dining/soups",
+    },
+    {
+      category: [],
+      title: "Mains",
+      subtitle: "10:00-23:00",
+      img: imgMains,
+      path: "/dining/mains",
+    },
+    {
+      category: [],
+      title: "Desserts",
+      subtitle: "24 hours",
+      img: imgDesserts,
+      path: "/dining/desserts",
+    },
+    {
+      category: [],
+      title: "Drinks",
+      subtitle: "24 hours",
+      img: imgDrinks,
+      path: "/dining/drinks",
+    },
+    {
+      category: [],
+      title: "Hot Drinks",
+      subtitle: "24 hours",
+      img: imgHotDrinks,
+      path: "/dining/hot-drinks",
+    },
+    {
+      category: [],
+      title: "Alcohol",
+      subtitle: "",
+      img: imgAlcohol,
+      path: "/dining/alcohol",
+    },
+  ]);
   return (
     <div className="page restaurants-bars-page">
-      <MenuVertical />
+      <Category title={t("menu.Dining")} list={list} />
+      <RightBottomButtons />
+      <LeftBottomButtons showNavigation={true} />
     </div>
   );
 }
--- seashell-react/src/app/pages/restaurants-bars.tsx
+++ seashell-react/src/app/pages/restaurants-bars.tsx
@@ -2,15 +2,15 @@ import { useState } from "react";
 import LeftBottomButtons from "../components/LeftBottomButtons";
 import Category from "../components/Category";
 import RightBottomButtons from "../components/RightBottomButtons";
+import { t } from "i18next";
 
 import imgLounge from "../../assets/categories/bars/lounge.jpg";
 import imgItablian from "../../assets/categories/bars/italian.jpg";
 import imgLobby from "../../assets/categories/bars/lobby.jpg";
 import imgAsia from "../../assets/categories/bars/asian.jpg";
-import { t } from "i18next";
 
 function RestaurantsBars() {
-  const [bars] = useState([
+  const [list] = useState([
     {
       title: "Lounge Bar",
       subtitle: "",
@@ -35,52 +35,10 @@ function RestaurantsBars() {
       img: imgAsia,
       path: "/restaurants-bars/asian-restaurant",
     },
-    {
-      title: "Lounge Bar 2",
-      subtitle: "",
-      img: imgLounge,
-      path: "/restaurants-bars/lounge-bar",
-    },
-    {
-      title: "The Italian Cafe 2",
-      subtitle: "",
-      img: imgItablian,
-      path: "/restaurants-bars/the-italian-cafe",
-    },
-    {
-      title: "Lobby Bar 2",
-      subtitle: "",
-      img: imgLobby,
-      path: "/restaurants-bars/lobby-bar",
-    },
-    {
-      title: "Asian Restaurant 2",
-      subtitle: "10:00-23:30",
-      img: imgAsia,
-      path: "/restaurants-bars/asian-restaurant",
-    },
-    {
-      title: "The Italian Cafe 3",
-      subtitle: "",
-      img: imgItablian,
-      path: "/restaurants-bars/the-italian-cafe",
-    },
-    {
-      title: "Lobby Bar 3",
-      subtitle: "",
-      img: imgLobby,
-      path: "/restaurants-bars/lobby-bar",
-    },
-    {
-      title: "Asian Restaurant 3",
-      subtitle: "10:00-23:30",
-      img: imgAsia,
-      path: "/restaurants-bars/asian-restaurant",
-    },
   ]);
   return (
     <div className="page restaurants-bars-page">
-      <Category title={t("menu.Restaurants & Bars")} list={bars} />
+      <Category title={t("menu.Restaurants & Bars")} list={list} />
       <RightBottomButtons />
       <LeftBottomButtons showNavigation={true} />
     </div>
